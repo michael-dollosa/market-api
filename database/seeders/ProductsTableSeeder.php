cfee386251e9270e29f3b629b3d9063837e61323
dolee <dollosa.christian@gmail.com>
2021-11-21 07:24:30 +0800

    completed client side sample CRUD for market-api

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -23,35 +23,35 @@ class ProductsTableSeeder extends Seeder
             'product_description' => 'Seed Description',
             'quantity' => 10,
             'price' => 999.98,
-            'status' => 'open'
+            'status' => 'enable'
         ]);
         Product::create([
             'product_name' => 'Test Product 2',
             'product_description' => 'Seed Description',
             'quantity' => 10,
             'price' => 999.98,
-            'status' => 'open'
+            'status' => 'disable'
         ]);
         Product::create([
             'product_name' => 'Test Product 3',
             'product_description' => 'Seed Description',
             'quantity' => 10,
             'price' => 999.98,
-            'status' => 'open'
+            'status' => 'enable'
         ]);
         Product::create([
             'product_name' => 'Test Product 4',
             'product_description' => 'Seed Description',
             'quantity' => 10,
             'price' => 999.98,
-            'status' => 'open'
+            'status' => 'enable'
         ]);
         Product::create([
             'product_name' => 'Test Product 5',
             'product_description' => 'Seed Description',
             'quantity' => 10,
             'price' => 999.98,
-            'status' => 'open'
+            'status' => 'disable'
         ]);
     }
 }
